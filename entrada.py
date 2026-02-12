6096cad8bd490e6c8c20c85dc27ed140e6005006
lliviamartins <lliviasantospaz2019@gmail.com>
2026-02-12 12:08:34 -0300

    atualizacao de entradas

---
+++ entrada.py
@@ -0,0 +1,6 @@
+nome=input('qual é o seu nome?')
+print ('Olá',nome,'seja bem vindo(a)')
+dominio=input('digite o seu dominio')
+print (dominio)
+email=nome+'@'+dominio
+print ('O seu e-mail é:',email)
